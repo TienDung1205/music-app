5aea3cd9b6bd5794c601869c2f83ad2a4a3cc273
Bùi Tiến Dũng <buitiendung12517@gmail.com>
2026-01-18 11:11:25 +0700

    Kết nối database

--- Source/server/index.ts
+++ Source/server/index.ts
@@ -1,1 +1,18 @@
-console.log("88881989");
+import express, {Express, Request, Response} from 'express';
+import dotenv from 'dotenv';
+import * as database from "./config/database";
+
+dotenv.config();
+
+database.connect();
+
+const app: Express = express();
+const port: number | string = process.env.PORT || 3000;
+
+app.get('/topics', (req: Request, res: Response) => {
+  res.json('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
