b075941459bad9525700fb6531ce6190531ec669
Bùi Tiến Dũng <buitiendung12517@gmail.com>
2026-01-18 11:30:46 +0700

    Lấy ra chủ đề bài hát

--- Source/server/index.ts
+++ Source/server/index.ts
@@ -2,6 +2,8 @@ import express, {Express, Request, Response} from 'express';
 import dotenv from 'dotenv';
 import * as database from "./config/database";
 
+import Topic from './api/v1/models/topic.model';
+
 dotenv.config();
 
 database.connect();
@@ -9,8 +11,14 @@ database.connect();
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
 
-app.get('/topics', (req: Request, res: Response) => {
-  res.json('Hello World!');
+app.get('/topics', async (req: Request, res: Response) => {
+  const topics = await Topic.find({
+    deleted: false
+  })
+
+  console.log(topics);
+
+  res.json(topics);
 });
 
 app.listen(port, () => {
