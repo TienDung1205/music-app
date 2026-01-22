e808be009e61cb585eec7ff91f7a5294f9f3bc31
Bùi Tiến Dũng <buitiendung12517@gmail.com>
2026-01-22 23:50:01 +0700

    Cài đặt cors

--- Source/server/index.ts
+++ Source/server/index.ts
@@ -1,5 +1,6 @@
 import express, {Express} from 'express';
 import dotenv from 'dotenv';
+import * as cors from 'cors';
 import * as database from "./config/database";
 
 import clientRoutesApiVer1 from './api/v1/routes/client/index.route';
@@ -10,6 +11,8 @@ database.connect();
 
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
+
+app.use(cors());
 
 clientRoutesApiVer1(app);
 
