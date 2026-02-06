b6b8e7c21c29452fd0a00612cf4ea912f949d07a
Bùi Tiến Dũng <buitiendung12517@gmail.com>
2026-02-06 20:45:06 +0700

    Fix search client

--- Source/server/api/v1/controllers/client/search.controller.ts
+++ Source/server/api/v1/controllers/client/search.controller.ts
@@ -1,10 +1,9 @@
 import { Request, Response } from "express";
-import Topic from "../../models/topic.model";
 import Song from "../../models/song.model";
 import Singer from "../../models/singer.model";
-import TopicSong from "../../models/topicsong.model";
-import SingerSong from "../../models/singersong.model";
 import { convertToSlug } from "../../../../helpers/convertToSlug";
+
+import * as songHelper from "../../../../helpers/song";
 
 // [GET] api/v1/search/:type
 export const result = async (req: Request, res: Response) => {
@@ -43,51 +42,9 @@ export const result = async (req: Request, res: Response) => {
 
             for(const song of songs){
                 // Tìm các Singer liên quan đến bài hát này
-                const singerSongs = await SingerSong.find({
-                    songId: String(song._id)
-                })
-                .sort({ order: 1 })
-                .lean();
+                const newSong = await songHelper.getSongWithSingers(song);
 
-                const singerIds = [
-                    ...new Set(singerSongs.map(ss => String(ss.singerId)))
-                ];
-
-                const singers = await Singer.find({
-                    _id: { $in: singerIds },
-                    status: "active",
-                    deleted: false
-                })
-                .select("fullName avatar slug")
-                .lean();
-
-                // // Tìm các Topic liên quan đến bài hát này
-                // const topicSongs = await TopicSong.find({
-                //     songId: String(song._id)
-                // })
-                // .lean();
-
-
-                // const topicIds = [
-                //     ...new Set(topicSongs.map(ts => String(ts.topicId)))
-                // ];
-
-                // const topics = await Topic.find({
-                //     _id: { $in: topicIds },
-                //     status: "active",
-                //     deleted: false
-                // })
-                // .select("title avatar slug")
-                // .lean();
-
-                const result = {
-                    ...song,
-                    singers
-                    // topics
-                };
-
-                newSongs.push(result);
-                // console.log("SONG:", result);
+                newSongs.push(newSong);
             }
 
             const singers = await Singer.find({
